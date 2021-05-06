85345979225f66bf6c13bd1a3c4d6bbb46a1fadb
Olof Salberger <olof.salberger@gmail.com>
2021-05-06 11:11:20 -0400

    got rid of globals and generated functions for now.

--- archetypal_storage.jl
+++ archetypal_storage.jl
@@ -1,17 +1,3 @@
-# Todo: figure out how to make this a generated closure.
-n = 0
-archetypes = Type[]
-@generated function archetype(t::Type)
-    global n
-    global archetypes
-    n+=1
-    push!(archetypes,t)
-    :($n)
-end
-
-empty_storage(::Type{Type{T}}) where {T} = Vector{T}()
-
-
 struct EntityLocation
     archetype::Int
     index::Int
@@ -21,23 +7,18 @@ mutable struct ArchetypalStorage
     entity_counter::Int
     entity_table::Vector{EntityLocation}
     archetypal_storage::Vector{Vector}
-end
-# n and archetypes should really be elements in this, not globals.
-# Need to figure out a way to do generated closures.
 
-function initialize_archetypes(s::ArchetypalStorage)
-    global n # TODO: Figure out how to get rid of those pesky globals.
-    if length(s.archetypal_storage) >= n
-        return
-    end
-    for i in (length(s.archetypal_storage) + 1):n
-        push!(s.archetypal_storage,empty_storage(archetypes[i]))
-    end
+    archetype_table::Vector{Type}
+    archetype_map::Dict{Type,Int}
 end
-function ArchetypalStorage() 
-    s = ArchetypalStorage(0,EntityLocation[],Vector[])
-    initialize_archetypes(s)
-    s
+ArchetypalStorage() = ArchetypalStorage(0,EntityLocation[],Vector[],Type[],Dict{Type,Int}())
+
+function get_archetype(s::ArchetypalStorage,t::Type{T}) where {T}
+    get!(s.archetype_map,t) do 
+        push!(s.archetype_table,t)
+        push!(s.archetypal_storage,T[])
+        length(s.archetype_table)
+    end
 end
 
 # (Need to determine whether entities should store their identity).
@@ -46,8 +27,7 @@ register_uid(entity,uid::Int) = entity
 function insert_entity!(storage::ArchetypalStorage,entity::T) where {T}
     storage.entity_counter += 1
     e = register_uid(entity,storage.entity_counter)
-    a = archetype(typeof(e))
-    initialize_archetypes(storage)
+    a = get_archetype(storage,typeof(e))
     push!(storage.archetypal_storage[a], e)
     location = EntityLocation(a,length(storage.archetypal_storage[a]))
     push!(storage.entity_table, location)
